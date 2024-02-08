1423e4a9559d4768be5a2eb083694d050593bdc9
ZanzibarNuclear <zanzisworld42@gmail.com>
2024-02-08 12:51:56 -0800

    Start setting up

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>This is on every page at the top.</header>
+  <slot />
+  <footer>&copy; Copyright 2024. This is on every page at the bottom.</footer>
+</template>
+
+<style scoped>
+.default {
+  border: 1px solid teal;
+  padding: 1rem;
+  position: relative;
+}
+header {
+  margin-top: 1rem;
+  margin-bottom: 3rem;
+}
+footer {
+  margin-top: 5rem;
+  margin-bottom: 1rem;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Hello, World of Nuclear!</h1>
+    <p>Today I am {{ age }} <button @click="older">+</button> years old.</p>
+  </div>
+</template>
+
+<script setup>
+const age = ref(4)
+const older = () => {
+  age.value += 1
+}
+</script>
+
+<style scoped>
+h1 {
+  color: teal;
+  margin-bottom: 1.5rem;
+  font-size: xx-large;
+}
+</style>
