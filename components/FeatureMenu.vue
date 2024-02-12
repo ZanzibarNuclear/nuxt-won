35e0b64742104264010822ca04307093e1a7d8af
ZanzibarNuclear <zanzisworld42@gmail.com>
2024-02-11 22:02:44 -0800

    Improve left nav

--- components/FeatureMenu.vue
+++ components/FeatureMenu.vue
@@ -8,24 +8,40 @@
 const links = [
   [
     {
-      label: 'What Say You',
+      label: 'Nuclear News',
+      icon: 'i-heroicons-newspaper',
+      to: 'nuclear-news',
     },
     {
-      label: 'Nuclear News',
+      label: 'What Say You',
+      icon: 'i-heroicons-chat-bubble-left-ellipsis',
+      to: 'what-say-you',
     },
     {
       label: 'Daily Lessons',
+      icon: 'i-heroicons-academic-cap',
     },
     {
       label: 'Simulators',
+      icon: 'i-heroicons-cog',
     },
     {
       label: 'Fun and Games',
+      icon: 'i-heroicons-puzzle-piece',
     },
   ],
   [
     {
       label: "Zanzi's Nuclear Stack",
+      icon: 'i-heroicons-document-text',
+    },
+    {
+      label: 'Zanzibar on X',
+      icon: 'i-ri-twitter-x-fill',
+    },
+    {
+      label: 'Discord Chat Channel',
+      icon: 'i-ic-baseline-discord',
     },
   ],
 ]
