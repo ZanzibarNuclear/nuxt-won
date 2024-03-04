bc858f512fc31e9f5bc5c37810777255133c225d
ZanzibarNuclear <zanzisworld42@gmail.com>
2024-03-03 21:42:20 -0800

    Setting up What Say You, forms, pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', 'nuxt-icon', '@nuxt/content'],
+  modules: ['@nuxt/ui', 'nuxt-icon', '@nuxt/content', '@pinia/nuxt'],
   content: {},
 })
--- pages/what-say-you.vue
+++ pages/what-say-you.vue
@@ -1,7 +1,40 @@
 <template>
-  <div>What Say You</div>
+  <div class="text-2xl font-bold">What Say You</div>
+  <UForm
+    :schema="schema"
+    :state="state"
+    class="mt-6 space-y-4"
+    @submit="onSubmit"
+  >
+    <UFormGroup label="Your pithy wisdom, or whatever" name="statement">
+      <UTextarea
+        v-model="state.statement"
+        placeholder="Go ahead and type something. Anything."
+        autoresize
+      />
+    </UFormGroup>
+    <UButton type="submit" label="Send" />
+  </UForm>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import { object, string, type InferType } from 'yup'
+import type { FormSubmitEvent } from '#ui/types'
+
+const schema = object({
+  statement: string().required('Say something, please.'),
+})
+
+type Schema = InferType<typeof schema>
+
+const state = reactive({
+  statement: undefined,
+})
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  alert(event.data)
+  console.log(event.data)
+}
+</script>
 
 <style lang="scss" scoped></style>
