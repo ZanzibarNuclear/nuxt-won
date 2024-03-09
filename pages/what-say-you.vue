e8bcf3e6eac0f1e0d253f726f90c45f16df8f2c9
ZanzibarNuclear <zanzisworld42@gmail.com>
2024-03-09 11:29:49 -0800

    Add "what say you" functionality in order to understand Nuxt UI better.

--- pages/what-say-you.vue
+++ pages/what-say-you.vue
@@ -9,17 +9,36 @@
     <UFormGroup label="Your pithy wisdom, or whatever" name="statement">
       <UTextarea
         v-model="state.statement"
+        name="statement"
         placeholder="Go ahead and type something. Anything."
         autoresize
       />
     </UFormGroup>
-    <UButton type="submit" label="Send" />
+    <UButton type="submit" label="Say it" />
+    <UButton color="blue" label="Take it back" @click="onTakeItBack" />
   </UForm>
+  <UButton
+    class="mt-8"
+    color="amber"
+    variant="solid"
+    label="Start over"
+    @click="onClear"
+  />
+
+  <div class="my-12 py-4 border-y">
+    <div class="text-2xl">These are the things you have said.</div>
+    <ul class="mt-6">
+      <li v-for="item in wsy.statements" key="index">{{ item }}</li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { object, string, type InferType } from 'yup'
 import type { FormSubmitEvent } from '#ui/types'
+import { useWsyStore } from '@/stores/wsyStore'
+
+const wsy = useWsyStore()
 
 const schema = object({
   statement: string().required('Say something, please.'),
@@ -32,9 +51,28 @@ const state = reactive({
 })
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
-  alert(event.data)
-  console.log(event.data)
+  const { statement } = event.data
+  wsy.makeStatement(statement)
+  state.statement = undefined
 }
+async function onTakeItBack() {
+  console.log('clicked button to remove last statement')
+  const whatWasSaid = wsy.takeItBack()
+  alert('You took back: ' + whatWasSaid)
+}
+async function onClear() {
+  console.log('clicked button to clear')
+  wsy.clear()
+}
+
+defineShortcuts({
+  meta_shift: {
+    usingInput: true,
+    handler: () => {
+      console.log('figure out how to submit form with this shortcut')
+    },
+  },
+})
 </script>
 
 <style lang="scss" scoped></style>
---
+++ stores/wsyStore.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+
+export const useWsyStore = defineStore('wsy', () => {
+  const statements = ref([])
+
+  const makeStatement = (statement) => {
+    statements.value.push(statement)
+  }
+
+  const takeItBack = () => {
+    const latest = statements.value.pop()
+    return latest
+  }
+
+  const clear = () => {
+    statements.value.splice(0, statements.value.length)
+  }
+
+  return { statements, makeStatement, takeItBack, clear }
+})
