d0139251dbdead6157042cedefabb1908af8fae0
ZanzibarNuclear <zanzisworld42@gmail.com>
2024-03-10 11:35:54 -0700

    Clean up what-say-you. Separate into edit and view components. Autofocus on textarea. Make new lines visible in viewer.

---
+++ components/WhatSayYouForm.vue
@@ -0,0 +1,83 @@
+<template>
+  <UForm
+    ref="wsyForm"
+    :schema="schema"
+    :state="state"
+    class="mt-6 space-y-4"
+    @submit="handleSubmit"
+  >
+    <UFormGroup label="Your pithy wisdom, or whatever" name="statement">
+      <UTextarea
+        autofocus
+        v-model="state.statement"
+        name="statement"
+        placeholder="Go ahead and type something. Anything."
+        autoresize
+      />
+    </UFormGroup>
+    <div class="relative mb-16">
+      <div class="absolute left-0 top-0">
+        <UButton class="mr-4" type="submit" label="Say it" />
+        <UButton color="blue" label="Take it back" @click="handleTakeItBack" />
+      </div>
+      <UButton
+        class="absolute right-0 top-0"
+        color="amber"
+        variant="solid"
+        label="Start over"
+        @click="handleClear"
+      />
+    </div>
+  </UForm>
+</template>
+
+<script setup lang="ts">
+import { object, string, type InferType } from 'yup'
+import type { FormSubmitEvent } from '#ui/types'
+import { useWsyStore } from '@/stores/wsyStore'
+
+const wsy = useWsyStore()
+const wsyForm = ref(null)
+
+const schema = object({
+  statement: string().required('Say something, please.'),
+})
+
+type Schema = InferType<typeof schema>
+
+const state = reactive({
+  statement: undefined,
+})
+
+async function handleSubmit(event: FormSubmitEvent<Schema>) {
+  const { statement } = event.data
+  wsy.makeStatement(statement)
+  state.statement = undefined
+}
+async function handleTakeItBack() {
+  const whatWasSaid = wsy.takeItBack()
+  console.log('You took back: ' + whatWasSaid)
+}
+async function handleClear() {
+  wsy.clear()
+}
+
+defineShortcuts({
+  meta_enter: {
+    usingInput: true,
+    handler: () => {
+      if (wsyForm.value) {
+        wsyForm.value.submit()
+      }
+    },
+  },
+  ctrl_z: {
+    usingInput: true,
+    handler: () => {
+      handleTakeItBack()
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/WhatSayYouViewer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="my-24 py-4 border-y">
+    <div class="text-2xl">These are the things you have said.</div>
+    <ul class="mt-6">
+      <li v-for="item in wsy.statements" key="index">
+        <span v-html="beautify(item)" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useWsyStore } from '@/stores/wsyStore'
+const wsy = useWsyStore()
+
+const beautify = (raw) => {
+  return raw.replaceAll(/\n/g, '<br/>')
+}
+</script>
+
+<style lang="scss" scoped>
+li {
+  padding-bottom: 1.5rem;
+  margin-bottom: 1rem;
+  border-bottom: 1px solid lightblue;
+}
+</style>
