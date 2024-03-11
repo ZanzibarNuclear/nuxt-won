b6865d59db9b964c053c1e0167194d2ad5f362cd
ZanzibarNuclear <zanzisworld42@gmail.com>
2024-03-10 20:53:38 -0700

    Testing: set up and got first real test to pass (after failing, so that worked, too).

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', 'nuxt-icon', '@nuxt/content', '@pinia/nuxt'],
+  modules: [
+    '@nuxt/ui',
+    'nuxt-icon',
+    '@nuxt/content',
+    '@pinia/nuxt',
+    '@nuxt/test-utils/module',
+  ],
   content: {},
 })
