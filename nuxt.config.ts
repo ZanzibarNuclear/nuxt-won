d2ed0a7b80c0e02bc27033d916e1bf8aa45ef6f9
ZanzibarNuclear <zanzisworld42@gmail.com>
2024-04-01 22:33:09 -0700

    stop redirect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,9 @@ export default defineNuxtConfig({
     '@nuxt/test-utils/module',
   ],
   content: {},
+  supabase: {
+    redirect: false,
+  },
   vite: {
     define: {
       global: 'window',
