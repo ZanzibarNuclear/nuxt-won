7d28d221407fb39e836399d9a6e17544f37eaccb
ZanzibarNuclear <zanzisworld42@gmail.com>
2024-04-07 07:34:43 -0700

    Improve settings view

--- components/UserProfileView.vue
+++ components/UserProfileView.vue
@@ -1,29 +1,49 @@
 <template>
-  <div class="border text-sm grid grid-cols-5 space-y-4">
-    <div class="label">Email</div>
-    <div class="col-span-4">me@domain.com</div>
-    <div class="label">Screen Name</div>
-    <div class="col-span-4">Blargy Pants Man</div>
-    <div class="label">Legal Name</div>
-    <div class="col-span-4">Mr. Blargy Pants, III</div>
-    <div class="label">About</div>
-    <div class="col-span-4">
-      I am the man of blargy who wears pants. Some call me Mr. Fancy Pants, but
-      that's not my real name. Frankly, I am insulted.
+  <UCard>
+    <div v-for="item in profile" class="mb-4 grid grid-cols-5">
+      <span class="label">{{ item.label }}</span>
+      <span class="col-span-4">{{ item.value }}</span>
     </div>
-    <div class="label">X</div>
-    <div class="col-span-4">@blargypantsman</div>
-    <div class="label">LinkedIn</div>
-    <div class="col-span-4">@blargypantsiii</div>
-    <div class="label">Discord</div>
-    <div class="col-span-4">blargypants</div>
-  </div>
+  </UCard>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const profile = ref([
+  {
+    label: 'Email',
+    value: 'me@domain.com',
+  },
+  {
+    label: 'Screen Name',
+    value: 'Blargy Pants Man',
+  },
+  {
+    label: 'Legal Name',
+    value: 'Mr. Blargy Pants, III',
+  },
+  {
+    label: 'About',
+    value:
+      "I am the man of blargy who wears pants. Some call me Mr. Fancy Pants, but that's not my real name. Frankly, I am insulted.",
+  },
+  {
+    label: 'X',
+    value: '@blargypantsman',
+  },
+  {
+    label: 'LinkedIn',
+    value: '@blargypantsiii',
+  },
+  {
+    label: 'Discord',
+    value: 'blargypants',
+  },
+])
+</script>
 
 <style scoped>
 .label {
   font-weight: bold;
+  margin-right: 3rem;
 }
 </style>
