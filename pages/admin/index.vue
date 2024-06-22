4b80efff3ca4a5543a7175fb4a082c46179e2c2e
Zanzibar Hero <zanzisworld42@gmail.com>
2024-06-22 09:33:00 -0700

    Use components for views. Arrange under tabs.

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,13 +1,34 @@
 <template>
   <UContainer class="mt-16">
     <h1>Admin Insights</h1>
-    <div>
+    <!-- <div>
       <NuxtLink to="/admin/invitations">Invitations</NuxtLink>
     </div>
     <div>
       <NuxtLink to="/admin/users">Members</NuxtLink>
+    </div> -->
+    <div class="my-8">
+      <UTabs :items="sections" class="w-full">
+        <template #members="{ item }">
+          <admin-member-viewer />
+        </template>
+        <template #invitations="{ item }">
+          <admin-invitation-viewer />
+        </template>
+      </UTabs>
     </div>
   </UContainer>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const sections = [
+  {
+    label: 'Members',
+    slot: 'members',
+  },
+  {
+    label: 'Invitations',
+    slot: 'invitations',
+  },
+]
+</script>
