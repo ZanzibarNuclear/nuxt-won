d010b71b76dd923706c14d14ef0a2804df91740e
Zanzibar Hero <zanzisworld42@gmail.com>
2024-06-28 07:37:35 -0700

    troubleshooting

--- server/api/admin/notify/feedback.post.ts
+++ server/api/admin/notify/feedback.post.ts
@@ -5,25 +5,41 @@ const config = useRuntimeConfig()
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
 
-  const { context, message } = body
+  const { context, message } = body.record
   const { adminEmail } = config
 
   const from = 'World of Nuclear (system) <system@support.worldofnuclear.com>'
   const subject = `(${context}) we got feedback. Hurray!!`
 
   const resend = new Resend(config.wsyInvitationsResendKey)
-  const { data, error: sendError } = await resend.emails.send({
-    from,
-    to: [adminEmail],
-    subject,
-    html: message,
-  })
 
-  if (sendError) {
-    console.error({ sendError })
-    return { success: false }
+  if (!message) {
+    const { data, error: sendError } = await resend.emails.send({
+      from,
+      to: [adminEmail],
+      subject,
+      html: message,
+    })
+    if (sendError) {
+      console.error({ sendError })
+      return { success: false }
+    } else {
+      console.log(data)
+      return { success: true }
+    }
   } else {
-    console.log(data)
-    return { success: true }
+    const { data, error: sendError } = await resend.emails.send({
+      from,
+      to: [adminEmail],
+      subject,
+      html: 'not quite: ' + JSON.stringify(body.record),
+    })
+    if (sendError) {
+      console.error({ sendError })
+      return { success: false }
+    } else {
+      console.log(data)
+      return { success: true }
+    }
   }
 })
