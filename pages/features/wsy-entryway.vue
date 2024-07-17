57440bc72aa1b0de4d2b2678c6112ea814f1570b
Zanzibar Hero <zanzisworld42@gmail.com>
2024-07-17 14:31:09 -0700

    Sketching out anonymous wsy viewer

---
+++ pages/features/wsy-entryway.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p v-for="thread in threads">
+      <NuxtLink :to="loadTopic(thread.publicKey)">{{ thread.topic }}</NuxtLink>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+let threads = ref([])
+
+async function loadData() {
+  const loadedThreads = await $fetch('/api/say/threads')
+  if (loadedThreads) {
+    loadedThreads.forEach((thread) => threads.value.push(thread))
+  }
+}
+await loadData()
+</script>
+
+<style scoped></style>
