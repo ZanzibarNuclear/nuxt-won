ad7ede75f65608ccd1edebf52871ea4cbb254589
Zanzibar <zanzisworld42@gmail.com>
2024-11-09 14:01:30 -0800

    Add Flux to the list of features

---
+++ pages/flux/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>What Say You?</h1>
+    <div>
+      Flux is all about sharing your thoughts and discovering what others are thinking. This is meant to be a safe place
+      to discuss nuclear energy.
+    </div>
+
+    <h2>Under Construction</h2>
+    <div class="my-4">
+      This feature is under construction. Try it out, and let us know what you think.
+    </div>
+    <div class="my-4">
+      <NuxtLink to="https://flux.worldofnuclear.com">Try Flux!!</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'World of Nuclear - Flux',
+  ogTitle: 'World of Nuclear - Flux',
+  description:
+    'Social media for ideas about nuclear energy. Share thoughts as they come to you. Start a chain reaction.',
+  ogDescription:
+    'Social media for ideas about nuclear energy. Share thoughts as they come to you. Start a chain reaction.',
+  ogImage: 'https://worldofnuclear.com/flux-theme-v1.jpg',
+  twitterCard: 'summary',
+})
+</script>
