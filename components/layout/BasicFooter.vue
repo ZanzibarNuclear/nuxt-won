584ba61ecc119d3d5b9d2baafee0fbab42c5cc9f
Zanzibar <zanzisworld42@gmail.com>
2025-01-20 14:47:45 -0800

    Update footer

--- components/layout/BasicFooter.vue
+++ components/layout/BasicFooter.vue
@@ -2,26 +2,22 @@
   <footer class="my-4 w-3/4 min-w-80">
     <div>
       <div class="flex flex-row justify-around text-heroic-cherenkov mb-4">
-        <div>
-          <UButton
-            to="https://hero.worldofnuclear.com"
-            icon="i-heroicons-home-modern"
-            label="Start"
-            variant="ghost"
-          />
-        </div>
-        <div>
-          <UButton
-            to="/guide"
-            icon="i-heroicons-map"
-            label="Guide"
-            variant="ghost"
-          />
-        </div>
         <UButton
-          to="/credits"
-          icon="i-heroicons-trophy"
-          label="Credits"
+          to="https://zanzisnuclearstack.substack.com/"
+          icon="i-ph-article"
+          label="Zanzi's Nuclear Stack"
+          variant="ghost"
+        />
+        <UButton
+          to="https://x.com/ZanzibarNuclear"
+          icon="i-ph-x-logo"
+          label="Zanzibar on X"
+          variant="ghost"
+        />
+        <UButton
+          to="https://davemount.substack.com/"
+          icon="i-ph-article"
+          label="Opinions and Observations"
           variant="ghost"
         />
       </div>
@@ -36,29 +32,29 @@
 </template>
 
 <script setup>
-const footerLinks = [
-  {
-    label: "Zanzi's Nuclear Stack",
-    icon: 'i-ph-article',
-    to: 'https://zanzisnuclearstack.substack.com/',
-    target: '_blank',
-    external: true,
-  },
-  {
-    label: 'Zanzibar on X',
-    icon: 'i-ph-x-logo',
-    to: 'https://x.com/ZanzibarNuclear',
-    target: '_blank',
-    external: true,
-  },
-  {
-    label: 'Opinions and Observations',
-    icon: 'i-ph-article',
-    to: 'https://davemount.substack.com/',
-    target: '_blank',
-    external: true,
-  },
-]
+// const footerLinks = [
+//   {
+//     label: "Zanzi's Nuclear Stack",
+//     icon: 'i-ph-article',
+//     to: 'https://zanzisnuclearstack.substack.com/',
+//     target: '_blank',
+//     external: true,
+//   },
+//   {
+//     label: 'Zanzibar on X',
+//     icon: 'i-ph-x-logo',
+//     to: 'https://x.com/ZanzibarNuclear',
+//     target: '_blank',
+//     external: true,
+//   },
+//   {
+//     label: 'Opinions and Observations',
+//     icon: 'i-ph-article',
+//     to: 'https://davemount.substack.com/',
+//     target: '_blank',
+//     external: true,
+//   },
+// ]
 </script>
 
 <style lang="scss" scoped></style>
