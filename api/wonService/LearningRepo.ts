9ced88fb6bdffd97ecfdd8ee07918ae9dc5ea7b6
Zanzibar <zanzisworld42@gmail.com>
2025-02-17 07:43:19 -0800

    Fill in API access for rest of learning

--- api/wonService/LearningRepo.ts
+++ api/wonService/LearningRepo.ts
@@ -3,12 +3,24 @@ import type {
   CreateCourseType,
   CourseBodyType,
   LessonPlanType,
-  LessonPathType
+  LessonPathType,
+  CreateLessonPathType,
+  LessonPathBodyType,
+  LessonStepType,
+  CreateLessonStepType,
+  CreateLessonPlanType,
+  LessonPlanBodyType,
+  CreateLessonContentType,
+  LessonContentBodyType,
+  LessonContentType
 } from "./schema"
 
 const won = useWonService()
 const coursePath = '/courses'
 const lessonPlanPath = '/lesson-plans'
+const lessonPathPath = '/lesson-paths'
+const lessonStepPath = '/lesson-steps'
+const contentPath = '/lesson-contents'
 
 // repositories/user.ts
 export const LearningRepository = {
@@ -49,11 +61,100 @@ export const LearningRepository = {
     return await won.delete(`${coursePath}/${key}`)
   },
 
-  async getLessonPlansForCourse(key: string): Promise<LessonPlanType> {
+  async getLessonPlansForCourse(key: string): Promise<LessonPlanType[]> {
     return await won.get(`${coursePath}/${key}/lesson-plans`)
   },
 
-  async getPathsForCourse(key: string): Promise<LessonPlanType> {
+  async getPathsForCourse(key: string): Promise<LessonPathType[]> {
     return await won.get(`${coursePath}/${key}/lesson-paths`)
   },
+
+  async getPath(key: string): Promise<LessonPathType> {
+    return await won.get(`${lessonPathPath}/${key}`)
+  },
+
+  async createPath(payload: CreateLessonPathType): Promise<LessonPathType> {
+    return await won.post(lessonPathPath, payload)
+  },
+
+  async updatePath(key: string, payload: LessonPathBodyType): Promise<LessonPathType> {
+    return await won.put(`${lessonPathPath}/${key}`, payload)
+  },
+
+  async deletePath(key: string) {
+    return await won.delete(`${lessonPathPath}/${key}`)
+  },
+
+  async getPathSteps(key: string): Promise<LessonStepType[]> {
+    return await won.get(`${lessonStepPath}/${key}`)
+  },
+
+  async getStep(key: string): Promise<LessonStepType> {
+    return await won.get(`${lessonStepPath}/${key}`)
+  },
+
+  async createStep(payload: CreateLessonStepType): Promise<LessonStepType> {
+    return await won.post(lessonStepPath, payload)
+  },
+
+  async updateStep(key: string, payload: LessonPathBodyType): Promise<LessonStepType> {
+    return await won.put(`${lessonStepPath}/${key}`, payload)
+  },
+
+  async deleteStep(key: string) {
+    return await won.delete(`${lessonStepPath}/${key}`)
+  },
+
+  async getLessonPlan(key: string): Promise<LessonPlanType> {
+    return await won.get(`${lessonPlanPath}/${key}`)
+  },
+
+  async createLessonPlan(payload: CreateLessonPlanType): Promise<LessonPlanType> {
+    return await won.post(lessonPlanPath, payload)
+  },
+
+  async updateLessonPlan(key: string, payload: LessonPlanBodyType): Promise<LessonPlanType> {
+    return await won.put(`${lessonPlanPath}/${key}`, payload)
+  },
+
+  async publishLessonPlan(key: string): Promise<LessonPlanType> {
+    return await won.put(`${lessonPlanPath}/${key}/publish`)
+  },
+
+  async unpublishLessonPlan(key: string): Promise<LessonPlanType> {
+    return await won.put(`${lessonPlanPath}/${key}/unpublish`)
+  },
+
+  async archiveLessonPlan(key: string): Promise<LessonPlanType> {
+    return await won.put(`${lessonPlanPath}/${key}/archive`)
+  },
+
+  async unarchiveLessonPlan(key: string): Promise<LessonPlanType> {
+    return await won.put(`${lessonPlanPath}/${key}/unarchive`)
+  },
+
+  async deleteLessonPlan(key: string) {
+    return await won.delete(`${lessonPlanPath}/${key}`)
+  },
+
+  async getContents(key: string): Promise<LessonContentType[]> {
+    return await won.get(`${lessonPlanPath}/${key}/contents`)
+  },
+
+  async getContentPart(key: string): Promise<LessonContentType> {
+    return await won.get(`${contentPath}/${key}`)
+  },
+
+  async createContentPart(payload: CreateLessonContentType): Promise<LessonContentType> {
+    return await won.post(contentPath, payload)
+  },
+
+  async updateContentPart(key: string, payload: LessonContentBodyType): Promise<LessonContentType> {
+    return await won.put(`${contentPath}/${key}`, payload)
+  },
+
+  async deleteContentPart(key: string) {
+    return await won.delete(`${contentPath}/${key}`)
+  },
+
 }
