69f4b04d9e80dca049c31c7f82bad23c17979703
Zanzibar <zanzisworld42@gmail.com>
2025-02-26 13:35:27 -0800

    rough in some structure that might work

---
+++ components/content/LessonSelector.vue
@@ -0,0 +1,20 @@
+<template>
+  <ClientOnly>
+    <div v-if="lesson">
+      <ContentRenderer v-if="lesson" :value="lesson" />
+    </div>
+  </ClientOnly>
+</template>
+
+<script lang="ts" setup>
+const lesson = ref()
+const load = async () => {
+  const { data } = await useAsyncData('benefits', () => {
+    return queryCollection('lessons').where('path', '=', '/lessons/why_nuclear').first()
+  })
+  lesson.value = data.value
+}
+await load()
+</script>
+
+<style></style>
