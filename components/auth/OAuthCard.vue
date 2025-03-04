480718220b925357f9dc29097009fd45522768a4
Zanzibar <zanzisworld42@gmail.com>
2025-03-04 10:51:34 -0800

    Adjust oauth options

--- components/auth/OAuthCard.vue
+++ components/auth/OAuthCard.vue
@@ -1,62 +1,18 @@
 <template>
   <UCard class="shadow-cherenkov bg-nuclear-50">
     <div class="space-x-2 space-y-4 text-center">
-      <UButton
-        icon="i-ph-google-logo-duotone"
-        size="lg"
-        variant="outline"
-        label="Google"
-        class="hover:bg-nuclear-100"
-        @click="signIn('google')"
-      />
-      <UButton
-        icon="i-ph-x-logo-duotone"
-        size="lg"
-        variant="outline"
-        label="X / Twitter"
-        class="hover:bg-nuclear-100"
-        @click="signIn('x')"
-      />
-      <UButton
-        icon="i-ph-github-logo-duotone"
-        size="lg"
-        variant="outline"
-        label="GitHub"
-        class="hover:bg-nuclear-100"
-        @click="signIn('github')"
-      />
-      <UButton
-        icon="i-ph-discord-logo-duotone"
-        size="lg"
-        variant="outline"
-        label="Discord"
-        class="hover:bg-nuclear-100"
-        @click="signIn('discord')"
-      />
-      <UButton
-        icon="i-ph-apple-logo-duotone"
-        size="lg"
-        variant="outline"
-        label="Apple"
-        class="hover:bg-nuclear-100"
-        @click="signIn('apple')"
-      />
-      <UButton
-        icon="i-ph-instagram-logo-duotone"
-        size="lg"
-        variant="outline"
-        label="Instagram"
-        class="hover:bg-nuclear-100"
-        @click="signIn('instagram')"
-      />
-      <UButton
-        icon="i-ph-linkedin-logo-duotone"
-        size="lg"
-        variant="outline"
-        label="LinkedIn"
-        class="hover:bg-nuclear-100"
-        @click="signIn('linkedin')"
-      />
+      <UButton icon="i-ph-google-logo-duotone" size="lg" variant="outline" label="Google" class="hover:bg-nuclear-100"
+        @click="signIn('google')" />
+      <UButton icon="i-ph-discord-logo-duotone" size="lg" variant="outline" label="Discord" class="hover:bg-nuclear-100"
+        @click="signIn('discord')" />
+      <UButton icon="i-ph-github-logo-duotone" size="lg" variant="outline" label="GitHub" class="hover:bg-nuclear-100"
+        @click="signIn('github')" />
+      <UButton icon="i-ph-x-logo-duotone" size="lg" variant="outline" label="X" class="hover:bg-nuclear-100"
+        @click="signIn('x')" />
+      <UButton icon="i-ph-apple-logo-duotone" size="lg" variant="outline" label="Apple" class="hover:bg-nuclear-100"
+        @click="signIn('apple')" />
+      <UButton icon="i-ph-instagram-logo-duotone" size="lg" variant="outline" label="Instagram"
+        class="hover:bg-nuclear-100" @click="signIn('instagram')" />
     </div>
     <template #footer>
       <div class="text-sm">
