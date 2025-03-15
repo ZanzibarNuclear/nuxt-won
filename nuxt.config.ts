2ca83f2a60411b9dcc3f4f9f326b845c66bcb0ea
Zanzibar <zanzisworld42@gmail.com>
2025-03-15 07:43:54 -0700

    Turn dev tools back on

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-01-23',
-  devtools: { enabled: false },
+  devtools: { enabled: true },
   sourcemap: true,
   colorMode: {
     preference: 'light'
