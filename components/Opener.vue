e947ea109be3c549c76137b873e439bfe422d01b
Zanzibar <zanzisworld42@gmail.com>
2025-03-17 09:47:39 -0700

    Improve entrance to site

--- components/Opener.vue
+++ components/Opener.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <NuxtLink :to="{ name: 'lobby-gallery' }">
-      <UCard>
-        <h1 class="font-semibold sm:text-5xl text-4xl text-center">Blue is the new green.</h1>
+      <UCard variant="subtle">
+        <h1 class="font-semibold sm:text-5xl text-4xl text-center my-0">Blue is the new green.</h1>
       </UCard>
     </NuxtLink>
   </div>
@@ -10,4 +10,9 @@
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+</style>
